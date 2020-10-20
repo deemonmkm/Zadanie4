eb65c0eb659152300ce16ea0a5e0aa6c7b4ed083
deemonmkm <deemon@k66.ru>
2020-10-20 21:56:11 +0500

    1

---
+++ 4/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace _4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.n_flight = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dest = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.b_time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.time_on_way = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.type_plane = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ticket_cost = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.number_seats = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.t_dest = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.procceds = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.n_flight,
+            this.dest,
+            this.b_time,
+            this.time_on_way,
+            this.type_plane,
+            this.ticket_cost,
+            this.number_seats,
+            this.t_dest,
+            this.procceds});
+            this.dataGridView1.Location = new System.Drawing.Point(438, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(944, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // n_flight
+            // 
+            this.n_flight.HeaderText = "Номер рейса";
+            this.n_flight.Name = "n_flight";
+            // 
+            // dest
+            // 
+            this.dest.HeaderText = "Конечный пункт";
+            this.dest.Name = "dest";
+            // 
+            // b_time
+            // 
+            this.b_time.HeaderText = "Время вылета";
+            this.b_time.Name = "b_time";
+            // 
+            // time_on_way
+            // 
+            this.time_on_way.HeaderText = "Время в пути";
+            this.time_on_way.Name = "time_on_way";
+            // 
+            // type_plane
+            // 
+            this.type_plane.HeaderText = "Тип самолета";
+            this.type_plane.Name = "type_plane";
+            // 
+            // ticket_cost
+            // 
+            this.ticket_cost.HeaderText = "Стоимость билета";
+            this.ticket_cost.Name = "ticket_cost";
+            // 
+            // number_seats
+            // 
+            this.number_seats.HeaderText = "Количество мест";
+            this.number_seats.Name = "number_seats";
+            // 
+            // t_dest
+            // 
+            this.t_dest.HeaderText = "Время прибытия";
+            this.t_dest.Name = "t_dest";
+            // 
+            // procceds
+            // 
+            this.procceds.HeaderText = "Выручка";
+            this.procceds.Name = "procceds";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(264, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Заполнить таблицу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(83, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(264, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Сортировка по конечному пункту";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(83, 149);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(264, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Отбор по типу самолета";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1459, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn n_flight;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dest;
+        private System.Windows.Forms.DataGridViewTextBoxColumn b_time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn time_on_way;
+        private System.Windows.Forms.DataGridViewTextBoxColumn type_plane;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ticket_cost;
+        private System.Windows.Forms.DataGridViewTextBoxColumn number_seats;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn t_dest;
+        private System.Windows.Forms.DataGridViewTextBoxColumn procceds;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ 4/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4
+{
+    public partial class Form1 : Form
+    {
+        struct Books
+        {
+            public string id;
+            public string name;
+            public int author;
+            public int genre;
+            public string price;
+            public int readers;
+            public int year;
+            public int l_date;
+            };
+
+        Books[] massiv;
+        public int n;
+
+        void Otbor()
+        {
+            dataGridView1.Rows.Clear();
+            string k = textBox1.Text;
+            foreach (var t in massiv)
+                if (t.type_plane == k)
+                {
+                    dataGridView1.Rows.Add(t.n_flight, t.dest, t.b_time, t.time_on_way, t.type_plane, t.ticket_cost, t.number_seats, t.time_dest, t.procceds);
+                }
+        }
+        int Time_dest(int a, int b)
+        {
+            return a + b;
+        }
+        int Proceeds(int a, int b)
+        {
+            return a * b;
+        }
+        public Form1()
+        {
+            string[] rows;
+            
+            InitializeComponent();
+
+            if (File.Exists("planes.txt"))
+            {
+                
+                StreamWriter writefl;
+                rows = File.ReadAllLines("planes.txt");
+                n = rows.Length;
+                massiv = new books[n];
+            }
+            else
+            {
+                MessageBox.Show("Файл не найден");
+                return;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                string[] str = rows[i].Split(';');
+                massiv[i].n_flight = str[0];
+                massiv[i].dest = str[1];
+                massiv[i].b_time = int.Parse(str[2]);
+                massiv[i].time_on_way = int.Parse(str[3]);
+                massiv[i].type_plane = str[4];
+                massiv[i].ticket_cost = int.Parse(str[5]);
+                massiv[i].number_seats = int.Parse(str[6]);
+                massiv[i].time_dest = Time_dest(massiv[i].b_time, massiv[i].time_on_way);
+                massiv[i].procceds = Proceeds(massiv[i].ticket_cost, massiv[i].number_seats);
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Print();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Sort();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Otbor();
+        }
+    }
+}
